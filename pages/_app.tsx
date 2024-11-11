0fd825ecd8db7ce2f9f02369b656ee20019540fb
sun-jay <sunny.jyrm@gmail.com>
2024-11-10 22:10:21 -0800

    template

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
+import Head from "next/head"
+import { Analytics } from '@vercel/analytics/react';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className='	sb_center'>
+      <Analytics />
+      <Head>
+        <title>Sunny Jayaram</title>
+        <meta name="description" content="Sunny Jayaram's Webside " />
+        <link rel="icon" href="IMG_8285 2-modified (1).png" />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className=" w-screen h-screen ">
+      <p className="z-20 pt-24">Hello</p>
+    </div>
+  );
+}
